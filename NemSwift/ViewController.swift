ad8f354a24e55abcf901a01b7d648c27180d6125
daoka <daoka.sweep@gmail.com>
2017-11-15 15:07:43 +0900

    Initial Commit

---
+++ NemSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NemSwift
+//
+//  Created by Kazuya Okada on 2017/11/15.
+//  Copyright © 2017年 OpenApostille. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
